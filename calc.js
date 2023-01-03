4d302f1445b0223b3722402b5c118a766c4c5b2e
Qosimjonov Jasurbek <108314430+Jasurbek1904@users.noreply.github.com>
2023-01-03 15:17:16 +0500

    Best calculator

---
+++ calc.js
@@ -0,0 +1,50 @@
+function bir() {
+    document.getElementById("nol").innerHTML = "1"
+    document.getElementById("nol").style.fontSize = "40px"
+    document.getElementById("nol").style.textAlign = "end"
+}
+function ikki() {
+    document.getElementById("nol").innerHTML = "2"
+      document.getElementById("nol").style.fontSize = "40px"
+    document.getElementById("nol").style.textAlign = "end"
+}
+function uch() {
+    document.getElementById("nol").innerHTML = "3"
+      document.getElementById("nol").style.fontSize = "40px"
+    document.getElementById("nol").style.textAlign = "end"
+}
+function tort() {
+    document.getElementById("nol").innerHTML = "4"
+      document.getElementById("nol").style.fontSize = "40px"
+    document.getElementById("nol").style.textAlign = "end"
+}
+function besh() {
+    document.getElementById("nol").innerHTML = "5"
+      document.getElementById("nol").style.fontSize = "40px"
+    document.getElementById("nol").style.textAlign = "end"
+}
+function olti() {
+    document.getElementById("nol").innerHTML = "6"
+      document.getElementById("nol").style.fontSize = "40px"
+    document.getElementById("nol").style.textAlign = "end"
+}
+function yetti() {
+    document.getElementById("nol").innerHTML = "7"
+      document.getElementById("nol").style.fontSize = "40px"
+    document.getElementById("nol").style.textAlign = "end"
+}
+function sakkiz() {
+    document.getElementById("nol").innerHTML = "8"
+      document.getElementById("nol").style.fontSize = "40px"
+    document.getElementById("nol").style.textAlign = "end"
+}
+function toqqiz() {
+    document.getElementById("nol").innerHTML = "9"
+      document.getElementById("nol").style.fontSize = "40px"
+    document.getElementById("nol").style.textAlign = "end"
+}
+function zero() {
+    document.getElementById("nol").innerHTML = "0"
+      document.getElementById("nol").style.fontSize = "40px"
+    document.getElementById("nol").style.textAlign = "end"
+}
